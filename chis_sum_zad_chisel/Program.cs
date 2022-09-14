52dbbc4f8d989389aabac7f4fdd57a1bffaaccd4
Alevtina <_@email.comgit>
2022-09-14 08:50:18 +0300

    Add summa cr v chisle

---
+++ chis_sum_zad_chisel/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+System.Console.WriteLine("Введите число");
+string num = Console.ReadLine();
+int[] array = new int[num.Length];
+for(int i = 0; i < num.Length; i++)
+{
+    array[i] = Convert.ToInt32(num[i].ToString());
+}
+Summa(array);
+
+void Summa(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        sum = sum + array[i];
+    }
+    System.Console.Write("Сумма цмфр в числе равна = ");
+    System.Console.Write(sum);
+}
+
+
+
