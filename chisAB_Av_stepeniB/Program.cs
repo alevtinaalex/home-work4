849c7633c9cdf1c5f6700953174a4af21739c231
Alevtina <_@email.comgit>
2022-09-14 08:46:14 +0300

    Add A v stepeni B

---
+++ chisAB_Av_stepeniB/Program.cs
@@ -0,0 +1,20 @@
+﻿//  Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+System.Console.WriteLine("Введите число А= ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число B= ");
+int b = Convert.ToInt32(Console.ReadLine());
+Stepen(a);
+
+void Stepen(int n)
+{
+    int n1=n;
+for(int step =1; step < b; step++)
+{
+    n = n*n1;
+}
+System.Console.WriteLine(n);
+}
